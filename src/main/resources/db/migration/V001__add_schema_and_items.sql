e301c48b5c84ba92b6550039acfdfc7c539425b3
Matti Tuomikoski <mtuomikoski@gmail.com>
2022-08-25 13:32:34 +0300

    Switch to Java 11, add initial migration

---
+++ src/main/resources/db/migration/V001__add_schema_and_items.sql
@@ -0,0 +1,16 @@
+CREATE SCHEMA IF NOT EXISTS packlister;
+
+CREATE TABLE IF NOT EXISTS accounts (
+    id       uuid PRIMARY KEY,
+    username text UNIQUE NOT NULL,
+    email    text UNIQUE NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS items (
+    id                uuid PRIMARY KEY,
+    name              text,
+    description       text,
+    weight            integer,
+    public_visibility boolean DEFAULT false,
+    account           uuid REFERENCES accounts
+);
